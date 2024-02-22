8d073994bf703ccd52689ac1ddd4accae13248b5
Emil Todirascu <emil.todirascu@gmail.com>
2024-02-22 20:02:48 +0100

    refactor edit.py

--- edit.py
+++ edit.py
@@ -12,26 +12,25 @@ def edit_clips(clip_paths):
     
     clips = []
     for clip_path in clip_paths:
-        overlay = make_overlay(clip_path)
-
         clip = VideoFileClip(clip_path)
+        overlay = make_overlay(clip_path)
         clip = add_overlay(clip, overlay)
         clips.append(clip)
 
-    trans = VideoFileClip("files/transitions/white noise.mp4").subclip(0, 0.5)
-    trans = trans.volumex(0.1)
+    transition_clip = VideoFileClip("files/transitions/white noise.mp4").subclip(0, 0.5)
+    transition_clip = transition_clip.volumex(0.1)
 
-    clip_times = get_clip_times(clips, trans.duration)
+    clip_times = get_clip_times(clips, transition_clip.duration)
     final_clip_path = add_data(clip_paths, clip_times)
     
-    final_clip = concatenate_videoclips(clips, transition=trans)
+    final_clip = concatenate_videoclips(clips, transition=transition_clip)
     final_clip.write_videofile(final_clip_path, preset="ultrafast")
 
     for clip in clips:
         clip.close()
     
+    transition_clip.close()
     final_clip.close()
-    trans.close()
 
     print("Edited clips.")
     return final_clip_path
@@ -39,13 +38,13 @@ def edit_clips(clip_paths):
 def get_clip_times(clips, transition_time):
     times = [0]
     output = ["00:00"]
-    for i in range(len(clips)):
+
+    for i in range(len(clips)-1):
         time = clips[i].duration + transition_time + times[i]
         times.append(int(time))
         output.append(f"{int(time//60):02d}:{int(time%60):02d}")
     
-    return output[:-1]
-
+    return output[:]
     
 def make_overlay(clip_path):
     with open(clip_path.replace(".mp4", ".json"), 'r', encoding="utf-8") as f:
@@ -66,7 +65,6 @@ def make_overlay(clip_path):
 
     return path
 
-
 def add_overlay(clip, overlay):
     img_clip = ImageClip(overlay).set_duration(5)
     edited_video = CompositeVideoClip([clip, img_clip])
@@ -75,7 +73,9 @@ def add_overlay(clip, overlay):
 def add_data(clip_paths, clip_times):
     with open(clip_paths[0].replace(".mp4", ".json"), 'r', encoding="utf-8") as f:
         file_data = json.load(f)
+
     game = file_data["game"]
+
     streamers_list = []
     keywords_list = []
     twitch_credits = "Credits:\n"
@@ -83,21 +83,22 @@ def add_data(clip_paths, clip_times):
     for i, clip_path in enumerate(clip_paths):
         with open(clip_path.replace(".mp4", ".json"), 'r', encoding="utf-8") as f:
             file_data = json.load(f)
+
         streamers_list.append(file_data["streamer"])
         keywords_list.extend(file_data["keywords"].split(", "))
+        chapters += f"{clip_times[i]} {file_data["title"]}\n"
 
         if file_data["streamer"] not in twitch_credits:
             twitch_credits += f"https://www.twitch.tv/{file_data['streamer']}\n"
-        chapters += f"{clip_times[i]} {file_data["title"]}\n"
 
     streamers = ", ".join(set(streamers_list))
     keywords = "#" + ", #".join(set(keywords_list))
-
     description = f"{chapters}\n\n\n{twitch_credits}\n\n\n{keywords}"
+
+    # Get the top 3 streamers by length of their names
     streamer_names = ", ".join(sorted(list(set(streamers_list)),key=lambda x: len(x))[:min(3, len(streamers_list))])
+
     title = f"🚨Top {game.upper()} Clips🚨{streamer_names} and more...🚨{datetime.datetime.today().date()}🚨"
-        
-    data_path = f"files/clips/{datetime.datetime.today().date()}/result/Top {game.upper()} Clips.json"
 
     data = {
         "title": title,
@@ -108,6 +109,8 @@ def add_data(clip_paths, clip_times):
         "streamer": streamers,
         "game": game
     }
+
+    data_path = f"files/clips/{datetime.datetime.today().date()}/result/Top {game.upper()} Clips.json"
 
     path = f"files/clips/{datetime.datetime.today().date()}/result/"
 
